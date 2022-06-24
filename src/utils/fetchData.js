787ae6da7f0d8beb5d8ff90b9427a2df7b17985c
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-06-24 10:39:31 -0500

    capturing error call async

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,5 +1,9 @@
-export const fetchData=async()=>{
-  const response = await fetch('https://restcountries.com/v3.1/all')
-  const data= await response.json()
-  return data;
-}
+export const fetchData = async () => {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
